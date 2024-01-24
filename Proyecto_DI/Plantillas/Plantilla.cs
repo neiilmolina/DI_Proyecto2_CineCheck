e13938d1591e0631851fa11bcc25d271a4c0674d
Neii <neilmolinarellano04@gmail.com>
2024-01-24 21:18:27 +0100

    07_version_plantilla

---
+++ Proyecto_DI/Plantillas/Plantilla.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_DI.Plantilla
+{
+    public class Plantilla : ContentPage
+    {
+        public Plantilla()
+        {
+            var plantilla = Application.Current.Resources["plantilla"] as ControlTemplate;
+            ControlTemplate = plantilla;
+        }
+    }
+}
