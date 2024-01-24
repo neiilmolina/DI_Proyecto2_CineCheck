21a5a39d36f0eb80112c0d5c6444c0b224d71536
Neii <neilmolinarellano04@gmail.com>
2024-01-24 21:05:12 +0100

    06.1_version_btnfav

--- Proyecto_DI/Template/Pelicula_Template.xaml.cs
+++ Proyecto_DI/Template/Pelicula_Template.xaml.cs
@@ -2,8 +2,25 @@ namespace Proyecto_DI.Template;
 
 public partial class Pelicula_Template : ContentView
 {
+    private bool favoritos = false;
+
 	public Pelicula_Template()
 	{
 		InitializeComponent();
 	}
+	/*
+    private void cambiarFavoritos(object sender, EventArgs e)
+    {
+
+        if (!favoritos)
+		{
+			favoritos = true;
+			btnFavoritos.BackgroundColor = "blue";
+        }
+		else 
+		{
+			favoritos = false;
+			btnFavoritos.BackgroundColor = new SolidColorBrush(Green);
+		}
+    } */
 }
